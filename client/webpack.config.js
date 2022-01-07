2cd9c5a2ed6c374e1e0060f611c8bd84c7a725e4
unknown <torinoutacnh@gmail.com>
2022-01-08 01:00:54 +0700

    Config webpack 1st

---
+++ client/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: ["@babel/polyfill", "./src/index.js"], // Dẫn tới file index.js ta đã tạo
+  output: {
+    path: path.join(__dirname, "/build"), // Thư mục chứa file được build ra
+    filename: "bundle.js" // Tên file được build ra
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Sẽ sử dụng babel-loader cho những file .js
+        exclude: /node_modules/, // Loại trừ thư mục node_modules
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.css$/, // Sử dụng style-loader, css-loader cho file .css
+        use: ["style-loader", "css-loader"]
+      },
+      { 
+        test: /\.(ttf|woff2?|png|jpe?g|svg|webp|gif|mp3)$/, 
+        exclude: /node_modules/, // Loại trừ thư mục node_modules
+        use: ['file-loader?name=[name].[ext]'] // ?name=[name].[ext] is only necessary to preserve the original file name
+      },
+    ]
+  },
+  // Chứa các plugins sẽ cài đặt trong tương lai
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      minify: {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeAttributeQuotes: true,
+          manifest: './public/manifest.json'
+      },
+      chunksSortMode: 'auto'
+    })
+  ],
+  // Dev server config
+  devServer: {
+    hot: true
+  },
+};
